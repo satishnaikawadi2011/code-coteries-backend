e7d9dcf44a2988cb2af9d38f77b9f86550e22931
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-10-04 13:28:00 +0530

    Added admin auth guard.

---
+++ src/guards/admin-auth.guard.ts
@@ -0,0 +1,34 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class AdminAuthGuard implements CanActivate {
+	constructor(private jwtService: JwtService, private usersService: UserService) {}
+
+	async canActivate(context: ExecutionContext) {
+		const ctx = GqlExecutionContext.create(context).getContext();
+		if (ctx.headers.Authorization) {
+			Object.assign(ctx.headers, { authorization: ctx.headers.Authorization });
+		}
+		if (ctx.headers.authorization && ctx.headers.authorization.startsWith('Bearer')) {
+			try {
+				const token = ctx.headers.authorization.split(' ')[1];
+				const decoded = await this.jwtService.verifyAsync(token, { secret: 'thisismysecret' });
+				const user = await this.usersService.findOne(decoded.id);
+				if (!user && !user.is_admin) {
+					return false;
+				}
+				ctx.userId = decoded.id;
+				return true;
+			} catch (err) {
+				console.log(err);
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+}
